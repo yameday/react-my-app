026e48fb3e2a621edad7e0867547e428c8bf0441
sakurakijp <sakurakijp@sakurakijpde-MacBook-Air.local>
2019-05-28 01:15:36 +0900

    init react myapp

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          QQQQ<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
